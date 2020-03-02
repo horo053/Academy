780c2927526bbc0df547f4d4c086b990f8922280
Kristina <yulfi555@gmail.com>
2020-03-02 20:06:25 +0300

    function min css

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
 const gulp = require('gulp');
 const browserSync = require('browser-sync').create();
+var cssmin = require('gulp-cssmin');
+const rename = require('gulp-rename');
 
 gulp.task('hello', function(done) {
   console.log('Привет, мир!');
@@ -14,4 +16,14 @@ gulp.task('browser-sync', function() {
       }
   });
   gulp.watch("./*.html").on('change', browserSync.reload);
+});
+
+
+// Min file
+gulp.task('gulp-rename', function (done) {
+  gulp.src('./css/*.css')
+      .pipe(cssmin())
+      .pipe(rename({suffix: '.min'}))
+      .pipe(gulp.dest('./css/'));
+    done();
 });
