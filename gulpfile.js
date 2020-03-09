0d76fe983437c9cff7161bf0d48677fe21655750
Kristina <yulfi555@gmail.com>
2020-03-09 13:29:00 +0300

    refact: make responsive layout fo header and hero

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ const browserSync = require('browser-sync').create();
 var cssmin = require('gulp-cssmin');
 const rename = require('gulp-rename');
 const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
 
 // Static server
 function bs() {
@@ -31,6 +32,9 @@ function gRename(done) {
 function serveSass() {
   return src("./sass/**/*.sass", "./sass/**/*.scss")
     .pipe(sass())
+    .pipe(autoprefixer({
+      cascade: false
+    }))
     .pipe(dest('./css'))
     .pipe(browserSync.stream());
 };
