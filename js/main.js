199768b2c79630fe4e6893bbf21d0a736c4472d3
Kristina <yulfi555@gmail.com>
2020-03-16 16:51:34 +0300

    feat: add button To beginning

--- js/main.js
+++ js/main.js
@@ -36,3 +36,19 @@ $(document).ready(function () {
         modal.toggleClass('modal--visible');
       });
 });
+
+$(document).ready(function(){
+  $(window).scroll(function(){
+    if ($(this).scrollTop() > 700) {
+        $('.scroll-up').fadeIn();
+    } else {
+        $('.scroll-up').fadeOut();
+    }
+    });
+      
+    $('.scroll-up').click(function(){
+    $("html, body").animate({ scrollTop: 0 }, 500);
+    return false;
+  });
+
+});
