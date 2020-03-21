b3ecca89e2203b654dfc7067b7d0431c82165637
Kristina <yulfi555@gmail.com>
2020-03-21 15:53:33 +0300

    Feat:connecting a mask to input fields

--- js/main.js
+++ js/main.js
@@ -57,6 +57,34 @@ $(document).ready(function () {
 
   new WOW().init();
 
+  $('.modal__form').validate({
+    errorClass: "invalid",
+    rules: {
+      userName: {
+        required: true,
+        minlength: 2
+      },
+      userPhone: "required",
+      userEmail: {
+        required: true,
+        email: true
+      }
+    },
+    messages: {
+      userName: {
+        required: "Обязательно укажите Имя",
+        minlength: "Имя должно быть не менее двух букв"
+      },
+      userPhone: "Телефон обязателен для ввода",
+      userEmail: {
+        required: "Обязательно укажите Email",
+        email: "Введите в формате: name@domain.com"
+      }
+    }
+  });
+
+  $('[type="tel"]').mask('+7(000)000-00-00', {placeholder: "+7(___)___-__-__"});
+
 });
 
 $(document).ready(function(){
