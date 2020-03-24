b4f56a791c63493cd24556ecb9442846c9dca776
Kristina <yulfi555@gmail.com>
2020-03-24 10:54:16 +0300

    fix: checkbox

--- js/main.js
+++ js/main.js
@@ -144,11 +144,11 @@ $(document).ready(function () {
     }
   });
 
-
-
   $('[type="tel"]').mask('+7(000)000-00-00', {placeholder: "+7(___)___-__-__"});
-
+  
 });
+
+
 
 $(document).ready(function(){
   $(window).scroll(function(){
--- send.php
+++ send.php
@@ -20,7 +20,7 @@ try {
     $mail->Host       = 'smtp.gmail.com';                       // Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
     $mail->Username   = 'yulfi555@gmail.com';                   // SMTP username
-    $mail->Password   = '';                       // SMTP password
+    $mail->Password   = 'yulfinordflesh';                       // SMTP password
     $mail->SMTPSecure = 'ssl';                                  // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
     $mail->Port       = 465;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
